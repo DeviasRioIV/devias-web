01ed03ae5b087ad98ce239356c95f383c4d8c070
nico-villegas <nicovillegas650@gmail.com>
2024-08-16 15:31:57 -0300

    Add roboto font with google font and delete .ttf file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  subsets: ['latin-ext'],
+  weight: ['100', '300', '400', '700'],
+  variable: '--font-roboto'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.variable}>{children}</body>
     </html>
   );
 }
