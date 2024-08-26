ec1841ecb42d5af4e27a15b74887d0499f9847e2
nico-villegas <nicovillegas650@gmail.com>
2024-08-26 17:42:08 -0300

    Add services card component

---
+++ src/app/Components/ServicesCard/ServicesCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Image from 'next/image'
+
+// Assets
+import './ServicesCard.scss'
+import servicesElement from 'Assets/Utilities/Elements/desktop/home-hero/services-card.png'
+
+export default function ServicesCard({ services }) {
+
+  console.log(services)
+  return (
+    <section id='services-card'>
+        <div className='container'>
+          <div className='container-dev-card'>
+            <div id="title-and-services">
+              <h2>
+                {services.title}
+              </h2>
+              <div className='services-card'>
+                <div id="services">
+                  <div className='services-description' id="design-side">
+                    <h3>
+                      {services.service_1}
+                    </h3>
+                    <h3>
+                      {services.service_2}
+                    </h3>
+                    <h3>
+                      {services.service_3}
+                    </h3>
+                  </div>
+                  <div className='services-description' id="dev-side">
+                    <h3>
+                      {services.service_4}
+                    </h3>
+                    <h3>
+                      {services.service_5}
+                    </h3>
+                    <h3>
+                      {services.service_6}
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Image src={servicesElement} alt='services' id='services-element'/>
+          </div>
+        </div>
+      </section>
+  )
+}
