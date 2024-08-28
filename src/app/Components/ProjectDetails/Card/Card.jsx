29260d2d4de9cfaab5dc565ce9754c6eea6d3b46
nico-villegas <nicovillegas650@gmail.com>
2024-08-28 17:07:46 -0300

    Add logic to placeholder image

--- src/app/Components/ProjectDetails/Card/Card.jsx
+++ src/app/Components/ProjectDetails/Card/Card.jsx
@@ -1,14 +1,18 @@
 // External modules
 import React from 'react'
+import Image from 'next/image'
 
 // Internal modules
 import './Card.scss'
+
+// Assets
+import imgPlaceholder from 'Assets/Projects/no-thumbnail.webp'
 
 export default function Card({img, alt, background}) {
 
   return (
     <div className='container-card' style={{background: background}}>
-      <img src={img} alt={alt} />
+      <Image src={img ? img : imgPlaceholder} alt={alt} width={300} height={300}/>
     </div>
   )
 
