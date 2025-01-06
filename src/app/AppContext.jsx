345cc5d4bbb182a995364b81184048c9cf72adbd
nico-villegas <nicovillegas650@gmail.com>
2025-01-06 18:21:30 -0300

    Remove unused AppContext and AppReducer files to streamline codebase

--- src/app/AppContext.jsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import React from 'react'
-
-export const AppContext = React.createContext()
-export const AppProvider = AppContext.Provider
-export const AppConsumer = AppContext.Consumer
